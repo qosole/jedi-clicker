9158b9e60c73de0fdc19d7ba3c43009092dd9b06
qosole <williamj99wei@gmail.com>
2022-08-03 15:04:12 -0500

    Add some item-seed data; Modify model relationship to use through table

--- seeds/item-seeds.js
+++ seeds/item-seeds.js
@@ -1,8 +1,50 @@
 const { Item } = require('../models');
 
 const itemData = [
+    // Equipment items
     {
-        
+        name: 'Training Stick',
+        price: 10,
+        strength: 1,
+        user_id: 1,
+        store_id: 1
+    },
+    {
+        name: 'Wampa Cloak',
+        price: 20,
+        strength: 2,
+        user_id: 1,
+        store_id: 1
+    },
+    {
+        name: 'Blaster',
+        price: 50,
+        strength: 10,
+        user_id: 1,
+        store_id: 1
+    },
+    {
+        name: 'Lightsaber',
+        price: 200,
+        strength: 60,
+        user_id: 1,
+        store_id: 1
+    },
+    {
+        name: 'Force Amplifier',
+        price: 5000,
+        strength: 2500,
+        user_id: 1,
+        store_id: 1
+    },
+
+    // Mercenaries
+    {
+        name: 'Vince Lee',
+        price: 1000000,
+        strength: 40000,
+        user_id: 1,
+        store_id: 2
     }
 ];
 
