7f87ae6187d2a80bb159adf483f8550ffd3850b8
qosole <williamj99wei@gmail.com>
2022-08-03 15:21:41 -0500

    Add seed data for Item

--- seeds/item-seeds.js
+++ seeds/item-seeds.js
@@ -6,44 +6,53 @@ const itemData = [
         name: 'Training Stick',
         price: 10,
         strength: 1,
-        user_id: 1,
         store_id: 1
     },
     {
         name: 'Wampa Cloak',
         price: 20,
         strength: 2,
-        user_id: 1,
         store_id: 1
     },
     {
         name: 'Blaster',
         price: 50,
         strength: 10,
-        user_id: 1,
         store_id: 1
     },
     {
         name: 'Lightsaber',
         price: 200,
         strength: 60,
-        user_id: 1,
         store_id: 1
     },
     {
         name: 'Force Amplifier',
         price: 5000,
         strength: 2500,
-        user_id: 1,
         store_id: 1
     },
 
-    // Mercenaries
+    // Mercenaries (can only be bought once)
     {
+        // Gives a certain discount to all items in both stores
         name: 'Vince Lee',
         price: 1000000,
         strength: 40000,
-        user_id: 1,
+        store_id: 2
+    },
+    {
+        // No special effect, just cost efficient
+        name: 'Kevin Hernandez',
+        price: 2000000,
+        strength: 300000,
+        store_id: 2
+    },
+    {
+        // Converts you to the dark side, increasing strength by a certain amount
+        name: 'Manuel Nunez',
+        price: 1000000000,
+        strength: 70000000,
         store_id: 2
     }
 ];
