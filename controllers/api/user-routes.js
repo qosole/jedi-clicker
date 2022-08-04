7251e8434fdb61d2f4e1013c1a98ca2a49b6c7f0
qosole <williamj99wei@gmail.com>
2022-08-04 15:46:50 -0500

    Add DELETE route for /api/users/:id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,8 +46,24 @@ router.post('/', async (req, res) => {
         }
 
         res.status(200).json(newUser);
-        
+
     } catch (err) { res.status(500).json(err); }
+});
+
+// Delete a user by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: { id: req.params.id }
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with that id' });
+            return;
+        }
+
+        res.status(200).json(userData);
+
+        } catch (err) { res.status(500).json(err); }
 });
 
 module.exports = router;
