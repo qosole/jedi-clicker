39912b58745a395506a4456c288ff8d2594ba277
qosole <williamj99wei@gmail.com>
2022-08-04 15:22:51 -0500

    Add POST route for /api/users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,4 +35,19 @@ router.get('/:id', async (req, res) => {
     } catch (err) { res.status(500).json(err); }
 });
 
+// Create a new user
+router.post('/', async (req, res) => {
+    try {
+        // req.body should have username, password, and galactic_credits = 0
+        const newUser = await User.create(req.body);
+        if (!newUser) {
+            res.status(400).json({ message: 'Syntax error with creating new user' });
+            return;
+        }
+
+        res.status(200).json(newUser);
+        
+    } catch (err) { res.status(500).json(err); }
+});
+
 module.exports = router;
