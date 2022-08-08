9c297213b51ee3b4718ffc902adba4b569c124e1
qosole <williamj99wei@gmail.com>
2022-08-08 13:53:24 -0500

    Remove unnecessary file

--- public/js/taken-username-alert.js
+++
@@ -1,6 +0,0 @@
-const takenUsernameAlert = document.querySelector('#taken-username-alert');
-
-// Removing the taken username alert after 3 seconds
-setTimeout(() => {
-    takenUsernameAlert.style.display = 'none';
-}, 3000)
